86dfa0e179116d840b1ddb497c4fa6808133f81d
Milan Pässler <milan@petabyte.dev>
2021-06-28 19:33:34 +0200

    parse cert in cert-auth service

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,26 @@ use hyper::service::{make_service_fn, service_fn};
 use futures_util::future::ok;
 use custom_error::custom_error;
 use std::env;
+use der_parser::oid;
+use x509_parser::pem::Pem;
+use x509_parser::prelude::X509Certificate;
+use x509_parser::extensions::{ParsedExtension, GeneralName};
 
 custom_error!{CustomError
        SystemTimeError{source: std::time::SystemTimeError} = "System time is before unix time",
        JWTError{source: jsonwebtoken::errors::Error} = "JWT Error: {source}",
        ToStrError{source: header::ToStrError} = "Error converting header value to string: {source}",
-       MissingHeader{name: String} = "Missing header: {name}",
-       MissingField{name: String} = "Missing field: {name}",
+       MissingHeader = "Missing x-ssl-client-escaped-cert header",
+       NoCertificate = "No certificate given",
+       InvalidSAN = "SAN exists but could not be parsed",
+       NoEmail = "Could not get email from certificate",
+       NoCommonName = "Certificate has no Common Name",
+       UrlEncoding{source: urlencoding::FromUrlEncodingError} = "Invalid urlencoding {source}",
+       PEM{source: x509_parser::prelude::PEMError} = "Decoding cert: {source}",
+       X509{source: x509_parser::prelude::X509Error} = "Decoding cert: {source}",
+       Nom{source: x509_parser::nom::Err<x509_parser::prelude::X509Error>} = "Decoding cert: {source}",
        HttpError{source: hyper::http::Error} = "HttpError: {source}",
-       ParseError = "Error while parsing",
+       Infallible{source: std::convert::Infallible} = "",
 }
 
 type Result<T> = std::result::Result<T, CustomError>;
@@ -29,23 +40,58 @@ struct Claims {
 	iat: u64,
 }
 
+fn get_email_from_san<'a>(cert: &'a X509Certificate) -> Result<Option<&'a str>> {
+    cert.extensions().get(&oid!(2.5.29.17)).and_then(|r| {
+        if let ParsedExtension::SubjectAlternativeName(san) = r.parsed_extension() {
+            // We got the SAN, maybe it has an email
+            san.general_names.iter().filter_map(|x| {
+                if let GeneralName::RFC822Name(email) = x {
+                    Some(Ok(email.to_owned()))
+                } else {
+                    None
+                }
+            }).next()
+        } else {
+            // This is supposed to be a SAN, but we can't parse it
+            Some(Err(CustomError::InvalidSAN))
+        }
+    }).transpose().map(|x| x)
+}
+
+fn get_email_from_dn<'a>(cert: &'a X509Certificate) -> Result<Option<&'a str>> {
+    cert.subject().iter_email().next()
+        .map(|x| Ok(x.as_str()?))
+        .transpose()
+}
+
+fn get_uid<'a>(cert: &'a X509Certificate) -> Result<Option<&'a str>> {
+    cert.subject().iter_by_oid(&oid!(0.9.2342.19200300.100.1.1)).next()
+        .map(|x| Ok(x.as_str()?))
+        .transpose()
+}
+
 fn process_request(req: Request<Body>) -> Result<Response<Body>> {
-	let dn = req.headers().get("x-ssl-client-dn").ok_or(CustomError::MissingHeader{name: "x-ssl-client-dn".to_string()})?.to_str()?;
-	let mut email = "";
-	let mut name = "";
-	let mut uid = "";
-	for pair in dn.split(',') {
-		let (key, _) = pair.split_at(pair.find('=').ok_or(CustomError::ParseError)?);
-		let (_, val) = pair.split_at(pair.find('=').ok_or(CustomError::ParseError)? + 1);
-		if key == "emailAddress" { email = val; }
-		if key == "CN" { name = val; }
-		if key == "UID" { uid = val; }
-	}
-	if email == "" { Err(CustomError::MissingField{name: "emailAddress".to_string()})? }
-	if name == "" { Err(CustomError::MissingField{name: "CN".to_string()})? }
-	if uid == "" {
-		uid = name;
-	}
+	let escaped_cert_str = req.headers().get("x-ssl-client-escaped-cert")
+        .ok_or(CustomError::MissingHeader)?.to_str()?;
+
+    let cert_str = urlencoding::decode(escaped_cert_str)?;
+
+    let cert_pem = Pem::iter_from_buffer(&cert_str.as_bytes()).next()
+        .ok_or(CustomError::NoCertificate)??;
+
+    let cert = cert_pem.parse_x509()?;
+
+    let email = get_email_from_san(&cert).transpose()
+        .or_else(|| get_email_from_dn(&cert).transpose())
+        .ok_or(CustomError::NoEmail)??;
+
+
+    let name = cert.subject().iter_common_name().next()
+        .map(|x| Ok::<_, CustomError>(x.as_str()?))
+        .transpose()?
+        .ok_or(CustomError::NoCommonName)?;
+
+	let uid = get_uid(&cert)?.unwrap_or(name);
 
 	let iat = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
 	let exp = iat + 3600;
